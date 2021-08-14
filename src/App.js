accecd72c653b21635ae13ab4e12782ce2a304fb
NIMRAH97 <nimrah.yousuf@gmail.com>
2021-08-15 01:23:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+//TODO implement debouncing with cancel token
 function App() {
+  let cancelToken = undefined;
+  let isInProgress = false;
+  let intervalVal=undefined;
+  const handleChange = async (e) => {
+    const type = e.target.value;
+    const a = setTimeout(async () => {
+      if (cancelToken !== undefined) {
+        cancelToken.cancel('Cancelling request');
+      }
+      cancelToken = axios.CancelToken.source();
+      const res = await axios.get('https://jsonplaceholder.typicode.com/comments?postId=1', {
+        cancelToken: cancelToken.token,
+      });
+      console.table(res.data);
+      console.log(type)
+      isInProgress = false;
+    }, 1000);
+    if (isInProgress) {
+      clearInterval(intervalVal);
+    }
+    isInProgress = true;
+    intervalVal = a;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={handleChange} type='text' placeholder='search here' />
+      <p></p>
     </div>
   );
 }
